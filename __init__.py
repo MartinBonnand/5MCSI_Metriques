6c2297b05c0b0a5eebb7e264df90c13bd30b45a3
MartinBonnand <martin1.bonnand@gmail.com>
2025-12-12 09:54:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)
 
-@app.route("/histogramme//")
+@app.route("/histogramme/")
 def mongraphique():
     return render_template("histogramme.html")
   
