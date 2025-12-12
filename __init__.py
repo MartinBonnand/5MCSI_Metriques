6b2f706de6d01c685ab1bd6eecc3f55c09479032
MartinBonnand <martin1.bonnand@gmail.com>
2025-12-12 09:43:15 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,10 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)
 
-
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+  
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
@@ -23,10 +26,7 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")                                                                                                                                       
+  
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
